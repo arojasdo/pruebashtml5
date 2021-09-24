2d39c44ec575913acb5abd39cd9605ff4b421cc7
arojasdo <arojas1123@gmail.com>
2021-09-23 20:48:58 -0500

    Todas las funciones

--- Paginas Responsive/pagina1/forms.js
+++ Paginas Responsive/pagina1/forms.js
@@ -31,6 +31,54 @@ function checkTelefono() {
 	}
 }
 
+function checkDir() {
+	valorDir = document.getElementById('direccion').value;
+	if(valorDir == null || valorDir.length == 0 || !/^[a-zA-Z0-9 \#\-]{1,50}$/.exec(valorDir)) {
+		console.log("Datos incorrectos");
+		return false;
+	}else{
+		console.log(valorDir);
+		return true;
+	}
+}
+
+function checkEmail() {
+	valorEmail = document.getElementById('email').value;
+	if(valorEmail == null || valorEmail.length == 0 || !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.exec(valorEmail)) {
+		console.log("Datos incorrectos");
+		return false;
+	}else{
+		console.log(valorEmail);
+		return true;
+	}
+}
+
+function checkContrasena() {
+	valorPass = document.getElementById('pass').value;
+	if(valorPass == null || valorPass.length < 8 || !/^[a-zA-Z0-9]{8,20}$/.exec(valorPass)) {
+		console.log("Datos incorrectos");
+		return false;
+	}else{
+		console.log(valorPass);
+		return true;
+	}
+}
+
+function checkGenero() {
+	opciones = document.getElementsByName("genero");
+	var seleccionado = false;
+	for(var i=0; i<opciones.length; i++) {
+		if(opciones[i].checked) {
+			seleccionado = true;
+			console.log("Seguro eres eso?");
+			break;
+		}
+	}
+	if(!seleccionado) {
+		console.log("seleccione una opcion");
+		return false;
+		}
+}
 
 
 
